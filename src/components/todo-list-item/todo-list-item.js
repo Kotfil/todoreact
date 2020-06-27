87d6055c5bc0716fe12ecbe69366855f844e92ec
FIlipp <filippkotenko@outlook.com>
2020-06-27 21:35:14 +0300

    todo4

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,37 +5,66 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+   state = {
+
+       done: false,
+       important: false
+   };
 
 
     getClick = () => {
-        console.log(`Done ${this.props.label}`)
+        this.setState(({done}) => {
+          return {
+              done: !done
+          }
+        })
+    }
+
+    onMarkImporant = () => {
+        this.setState(({important}) => {
+            return {
+                important: !important
+            }
+        })
     }
 
     render() {
 
-        const {label, important = false} = this.props;
+        const {label} = this.props;
+        const {done,important} = this.state;
 
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
 
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+        if(important) {
+            classNames += ' important';
+        }
+
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
       <span
           className="todo-list-item-label"
-          style={style}
+
           onClick={this.getClick}>
         {label}
       </span>
 
       <button type="button"
+              onClick={this.onMarkImporant}
               className="btn btn-outline-success btn-sm float-right">
+          *
         <i className="fa fa-check" aria-hidden="true"/>
       </button>
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right">
+          del
         <i className="fa fa-scissors" aria-hidden="true"/>
       </button>
     </span>
