b2bc5d04d64b544658069e4d3e5de684cf047153
FIlipp <filippkotenko@outlook.com>
2020-06-28 21:07:50 +0300

    todo5

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-
 import AppHeader from './../app-header';
 import SearchPanel from './../search-panel';
 import TodoList from './../todo-list';
 import ItemStatusFilter from './../item-status-filter';
-
 import './App.css';
 
 const App = () => {
@@ -14,7 +12,6 @@ const App = () => {
         { label: 'Make Awesome App', important: true, id: 2 },
         { label: 'Have a lunch', important: false, id: 3 }
     ];
-
     return (
         <div className="todo-app">
             <AppHeader toDo={1} done={3} />
@@ -22,10 +19,10 @@ const App = () => {
                 <SearchPanel />
                 <ItemStatusFilter />
             </div>
-
-            <TodoList todos={todoData} />
+            <TodoList
+                onDeleted={(id)  => console.log('Del',id)}
+                todos={todoData} />
         </div>
     );
 };
-
 export default App
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,21 +2,17 @@ import React, {Component} from 'react';
 
 import './todo-list-item.css';
 
-
 export default class TodoListItem extends Component {
-
-   state = {
-
-       done: false,
-       important: false
-   };
-
+    state = {
+        done: false,
+        important: false
+    };
 
     getClick = () => {
         this.setState(({done}) => {
-          return {
-              done: !done
-          }
+            return {
+                done: !done
+            }
         })
     }
 
@@ -29,47 +25,40 @@ export default class TodoListItem extends Component {
     }
 
     render() {
-
-        const {label} = this.props;
-        const {done,important} = this.state;
-
+        const {label, onDeleted} = this.props;
+        const {done, important} = this.state;
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
-
         let classNames = 'todo-list-item';
         if (done) {
             classNames += ' done';
         }
-        if(important) {
+        if (important) {
             classNames += ' important';
         }
-
         return (
             <span className={classNames}>
-      <span
-          className="todo-list-item-label"
-
-          onClick={this.getClick}>
+             <span
+                 className="todo-list-item-label"
+                 onClick={this.getClick}>
         {label}
       </span>
-
-      <button type="button"
-              onClick={this.onMarkImporant}
-              className="btn btn-outline-success btn-sm float-right">
+                <button type="button"
+                        onClick={this.onMarkImporant}
+                        className="btn btn-outline-success btn-sm float-right">
           *
         <i className="fa fa-check" aria-hidden="true"/>
       </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
+                <button type="button"
+                        onClick={onDeleted}
+                        className="btn btn-outline-danger btn-sm float-right">
           del
         <i className="fa fa-scissors" aria-hidden="true"/>
       </button>
     </span>
         );
-
     }
 }
 
